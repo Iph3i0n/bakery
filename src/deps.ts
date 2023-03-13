e5211cbd2ffd0e6ba3e16c8a0a1352080aef09ef
Paul Popat <paul@popatux.co.uk>
2023-03-13 08:57:40 +0000

    Updated wholemeal

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/wholemeal@0.1.10/mod.ts";
+export * from "https://deno.land/x/wholemeal@1.0.0/mod.ts";
 export { default as ConvertSize } from "https://x.nest.land/convert-size@1.1.8/mod.ts";
