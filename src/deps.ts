4af8ec545a6df26881b311a2231e307fff2e7ba1
Paul Popat <paul@popatux.co.uk>
2023-03-16 09:25:29 +0000

    Started switch to better event system

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/wholemeal@1.0.2/mod.ts";
+export * from "https://deno.land/x/wholemeal@1.0.4/mod.ts";
 export { default as ConvertSize } from "https://x.nest.land/convert-size@1.1.8/mod.ts";
