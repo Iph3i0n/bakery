ee71773925d730ac165de816e400bee25335f95c
Paul Popat <paul@popatux.co.uk>
2023-04-17 14:30:55 +0000

    Fixed numeric with prefill

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/wholemeal@1.0.5/mod.ts";
+export * from "https://deno.land/x/wholemeal@1.0.6/mod.ts";
 export { default as ConvertSize } from "https://x.nest.land/convert-size@1.1.8/mod.ts";
