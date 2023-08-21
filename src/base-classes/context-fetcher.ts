3911f0f9fa1ce0733abd4f2757cedfa26af236ab
Paul Popat <paul@popatux.co.uk>
2023-08-21 10:27:06 +0000

    Added edge cases for failing data

--- src/base-classes/context-fetcher.ts
+++ src/base-classes/context-fetcher.ts
@@ -2,11 +2,16 @@ import BakeryBase from "./main.ts";
 
 export default abstract class ContextFetcher extends BakeryBase {
   use_string_context(prop_name: string) {
-    // deno-lint-ignore no-explicit-any
-    const checker = (this as any)[prop_name];
+    try {
+      // deno-lint-ignore no-explicit-any
+      const checker = (this as any)[prop_name];
 
-    if (typeof checker !== "string" || !checker.startsWith(":")) return checker;
-    const fetcher = new Function("return " + checker.replace(":", ""));
-    return fetcher.call(this);
+      if (typeof checker !== "string" || !checker.startsWith(":"))
+        return checker;
+      const fetcher = new Function("return " + checker.replace(":", ""));
+      return fetcher.call(this);
+    } catch {
+      return undefined;
+    }
   }
 }
