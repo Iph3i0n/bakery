66ffea237e23a3720ca2d66191b162273eea7dea
Paul Popat <paul@popatux.co.uk>
2023-09-27 09:57:38 +0000

    added alert tests

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,12 +1,58 @@
-import { it, afterAll, beforeAll, describe } from "@bdd";
+import { it as org, afterAll, beforeAll, describe } from "@bdd";
 import Puppeteer, { Browser, Page } from "@puppeteer";
 import * as Path from "@path";
 import { assertSnapshot } from "@snapshot";
+import { assert } from "@assert";
 const __dirname = Path.dirname(Path.fromFileUrl(import.meta.url));
+
+const screenshot_dir = "./__screens__";
+
+const colours = [
+  "body",
+  "headline",
+  "surface",
+  "primary",
+  "contrast",
+  "warning",
+  "rainbow_0",
+  "rainbow_1",
+  "rainbow_2",
+  "rainbow_3",
+  "rainbow_4",
+  "rainbow_5",
+  "rainbow_6",
+  "rainbow_7",
+  "rainbow_8",
+  "rainbow_9",
+] as const;
 
 describe("App", () => {
   let browser: Browser;
   let page: Page;
+
+  const it = (name: string, imp: (t: Deno.TestContext) => Promise<void>) => {
+    org(name, async (t) => {
+      let c: Deno.TestContext | undefined = t;
+      const tests = [];
+      while (c) {
+        tests.push(c);
+        c = c.parent;
+      }
+
+      const name = tests
+        .map((t) => t.name)
+        .reverse()
+        .join(" - ");
+      try {
+        await imp(t);
+      } finally {
+        await Deno.mkdir(screenshot_dir, { recursive: true });
+        await page.screenshot({
+          path: Path.join(screenshot_dir, name + ".png"),
+        });
+      }
+    });
+  };
 
   beforeAll(async () => {
     browser = await Puppeteer.launch();
@@ -69,11 +115,58 @@ describe("App", () => {
     }, selector);
   }
 
+  async function deep_click(...selectors: Array<string>) {
+    const handle = await page.evaluateHandle(
+      (...selectors) =>
+        selectors.slice(1).reduce((c, n) => {
+          const result = c?.shadowRoot?.querySelector(n);
+          if (!result) throw new Error("Cannot find handle");
+          return result;
+        }, document.querySelector(selectors[0])),
+      ...selectors
+    );
+
+    await handle.click();
+  }
+
+  async function exists(selector: string) {
+    try {
+      await page.$(selector);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  async function wait_for_destroy(selector: string) {
+    await page.waitForFunction(`!document.querySelector('${selector}')`);
+  }
+
   describe("d-alert", () => {
     it("Draws an alert", async (t) => {
       await load(`<d-alert>Test Alert</d-alert>`);
       await page.waitForSelector("d-alert");
       await assertSnapshot(t, await element_snapshot("d-alert"));
+    });
+
+    for (const colour of colours)
+      it(`Draws a ${colour} alert`, async (t) => {
+        await load(`<d-alert colour="${colour}">Test Alert</d-alert>`);
+        await page.waitForSelector("d-alert");
+        await assertSnapshot(t, await element_snapshot("d-alert"));
+      });
+
+    it("Draws a close button", async (t) => {
+      await load(`<d-alert closeable>Test Alert</d-alert>`);
+      await page.waitForSelector("d-alert");
+      await assertSnapshot(t, await element_snapshot("d-alert"));
+    });
+
+    it("Closes on a close button", async () => {
+      await load(`<d-alert closeable>Test Alert</d-alert>`);
+      await page.waitForSelector("d-alert");
+      await deep_click("d-alert", ".close-button");
+      await wait_for_destroy("d-alert");
     });
   });
 });
